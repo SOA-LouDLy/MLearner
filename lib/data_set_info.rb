0f95c30a5882de70cb40ad9d2f53b358f9ae2f0d
Malherbe <malherbe.billy@gmail.com>
2021-10-24 15:50:51 +0800

    change to data/2 and retreive new data

--- lib/data_set_info.rb
+++ lib/data_set_info.rb
@@ -17,19 +17,17 @@ oml_response = {}
 oml_results = {}
 
 ## HAPPY DATA INFO REQUEST
-data_info_url = oml_api_path('data/1', config)
+data_info_url = oml_api_path('data/2', config)
 oml_response[data_info_url] = call_oml_url(data_info_url)
 data_info = JSON.parse(oml_response[data_info_url].body.to_s)
 
-oml_results['name'] = data_info['data_set_description']['name']
+oml_results['creator'] = data_info['data_set_description']['creator']
+
+oml_results['url'] = data_info['data_set_description']['url']
 
 oml_results['description'] = data_info['data_set_description']['description']
 
-oml_results['format'] = data_info['data_set_description']['format']
-
-oml_results['tag'] = data_info['data_set_description']['tag']
-
-oml_results['original_data_url'] = data_info['data_set_description']['original_data_url']
+oml_results['name'] = data_info['data_set_description']['name']
 
 ## BAD DATA INFO REQUEST
 bad_data_info_url = oml_api_path('data/99999999', config)
